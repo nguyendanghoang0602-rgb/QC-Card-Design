51c96d1e4b04b3348056ed67a391f4ecb87fdec7
nguyendanghoang0602-rgb <nguyendanghoang0602@gmail.com>
2026-04-17 17:04:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({mode}) => {
   return {
     base: '/QCCardDesign/',
     server: {
-      port: 3000;
+      port: 3000,
       host: '0.0.0.0',
     },
     plugins: [react(), tailwindcss()],
